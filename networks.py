86aab2ddb236d2923f7860d8065192adef0ad5d9
alanraydan <alan.raydan@gmail.com>
2023-12-25 21:54:22 -0800

    Class embedding layer implemented in `Generator` and `Descriminator`

--- networks.py
+++ networks.py
@@ -11,15 +11,17 @@ class Generator(nn.Module):
     """
     Generator network for conditional WGAN.
     
-    Input: (batch_size, latent_dim)
-    Output: (batch_size, 3, 268, 182)
+    Output image size: (batch_size, 3, 268, 182)
     """
-    def __init__(self, latent_dim, n_classes):
+    def __init__(self, latent_dim, n_classes, class_embedding_dim):
         super(Generator, self).__init__()
         self.latent_dim = latent_dim
         self.n_classes = n_classes
+        self.class_embedding_dim = class_embedding_dim
 
-        self.input_layer = nn.Linear(self.latent_dim + self.n_classes, 128*30*20)
+        self.class_embedding_layer = nn.Linear(self.n_classes, self.class_embedding_dim, bias=False) # No bias term to emulate nn.Embedding
+
+        self.input_layer = nn.Linear(self.latent_dim + self.class_embedding_dim, 128*30*20)
 
         self.deconv1 = nn.Sequential(
             nn.ConvTranspose2d(128, 128, kernel_size=(6, 5), stride=2),
@@ -36,9 +38,10 @@ class Generator(nn.Module):
             nn.Tanh()
         )
     
-    def forward(self, z, class_embedding):
+    def forward(self, z, class_multihot):
         assert z.shape == (z.shape[0], self.latent_dim), 'Incorrect latent variable shape.'
-        assert class_embedding.shape == (class_embedding.shape[0], self.n_classes), 'Incorrect class embedding shape.'
+        assert class_multihot.shape[1] == self.n_classes, 'Incorrect class embedding shape.'
+        class_embedding = self.class_embedding_layer(class_multihot)
         x = torch.cat((z, class_embedding), dim=1)
         x = self.input_layer(x)
         x = x.view(x.size(0), 128, 30, 20)  # Reshape
@@ -49,14 +52,14 @@ class Generator(nn.Module):
         return x
 
     @torch.no_grad()
-    def generate_poster(self, class_embedding, z=None):
+    def generate_poster(self, class_multihot, z=None):
         """
         Generate a movie poster given a class embedding.
         """
-        assert class_embedding.shape == (class_embedding.shape[0], self.n_classes), 'Incorrect class embedding shape.'
+        assert class_multihot.shape[1] == self.n_classes, 'Incorrect class embedding shape.'
         if z is None:
             z = torch.randn(1, self.latent_dim)
-        poster = self.forward(z, class_embedding)
+        poster = self.forward(z, class_multihot)
         poster = (poster + 1) * 127.5
         poster = transforms.functional.to_pil_image(poster.squeeze(0).type(torch.uint8))
         return poster
@@ -66,12 +69,14 @@ class Discriminator(nn.Module):
     """
     Discriminator network for conditional WGAN.
 
-    Input: (batch_size, 3, 268, 182)
-    Output: (batch_size, 1)
+    Input image size: (batch_size, 3, 268, 182)
     """
-    def __init__(self, n_classes):
+    def __init__(self, n_classes, class_embedding_dim):
         super(Discriminator, self).__init__()
         self.n_classes = n_classes
+        self.class_embedding_dim = class_embedding_dim
+
+        self.class_embedding_layer = nn.Linear(self.n_classes, self.class_embedding_dim, bias=False) # No bias term to emulate nn.Embedding
 
         self.conv1 = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=6, stride=2),
@@ -89,12 +94,13 @@ class Discriminator(nn.Module):
         )
         self.output_layer = nn.Linear(64*30*20 + self.n_classes, 1)
     
-    def forward(self, x, class_embedding):
+    def forward(self, x, class_multihot):
         assert x.shape == (x.shape[0], 3, 268, 182), 'Incorrect image shape.'
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
 
+        class_embedding = self.class_embedding_layer(class_multihot)
         x = x.view(x.size(0), -1)  # Flatten
         x = torch.cat((x, class_embedding), dim=1)
         x = self.output_layer(x)
