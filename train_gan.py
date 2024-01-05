3bb89c943024450fc28cc2826e88f9c402db0c52
alanraydan <alan.raydan@gmail.com>
2024-01-04 19:45:09 -0800

    Implemented training from checkpoint

--- train_gan.py
+++ train_gan.py
@@ -8,17 +8,21 @@ from trainer import Trainer
 from data_loader import PosterDataset
 
 if __name__ == '__main__':
+    start_from_checkpoint = False
+    checkpoint_path = '/Users/alanraydan/Development/PosterGAN/runs/Dec28_23-09-05_Alans-MacBook-Pro-2.local'
+
     # Taken from WGAN gradient penalty paper
     lr = 0.0001
     betas = (0.0, 0.9)
-    lambda_gp = 10
+    lambda_gp = 20
     g_update_freq = 5
 
     batch_size = 64
-    epochs = 5
+    epochs = 60
     latent_dim = 100
     class_embedding_dim = 16
     device = 'mps' if torch.backends.mps.is_available() else 'cpu'
+    global_step = 0
 
     dataset = PosterDataset()
     train_size = int(1.0 * len(dataset))
@@ -27,17 +31,27 @@ if __name__ == '__main__':
     dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
     G = Generator(latent_dim=latent_dim, n_classes=dataset.n_genres, class_embedding_dim=class_embedding_dim)
-    G.to(device)
     D = Discriminator(n_classes=dataset.n_genres, class_embedding_dim=class_embedding_dim)
-    D.to(device)
+    G = G.to(device)
+    D = D.to(device)
+
     optimizer_G = torch.optim.Adam(G.parameters(), lr=lr, betas=betas)
     optimizer_D = torch.optim.Adam(D.parameters(), lr=lr, betas=betas)
+
+    if start_from_checkpoint:
+        G_checkpoint = torch.load(f'{checkpoint_path}/G.pt')
+        D_checkpoint = torch.load(f'{checkpoint_path}/D.pt')
+        G.load_state_dict(G_checkpoint['model_state_dict'])
+        D.load_state_dict(D_checkpoint['model_state_dict'])
+        optimizer_G.load_state_dict(G_checkpoint['optimizer_state_dict'])
+        optimizer_D.load_state_dict(D_checkpoint['optimizer_state_dict'])
+        global_step = G_checkpoint['global_step']
 
     writer = SummaryWriter()
 
     trainer = Trainer(G, D, optimizer_G, optimizer_D,
                     gp_weight=lambda_gp, update_freq_G=g_update_freq,
-                    device=device, writer=writer)
+                    device=device, writer=writer, global_step=global_step)
 
     trainer.train(epochs, dataloader, save_networks=True)
     writer.close()
